a050cdac0c756c36fb51de135fd9ea0696509263
andestaerfan <andesta.erfan@gmail.com>
2020-02-19 14:19:48 +0330

    - Changed API error enum

--- iOS-Challenge/Modules/Authorization/ViewModels/AuthorizationViewModel.swift
+++ iOS-Challenge/Modules/Authorization/ViewModels/AuthorizationViewModel.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Alamofire
 
-enum AuthorizationError: Error {
+enum APIError: Error {
     case failedRequest
     case notURL
 }
@@ -19,18 +19,18 @@ class AuthorizationViewModel {
     
     //MARK: - Functions -
     
-    func login(completionHandler: (Swift.Result<URL, AuthorizationError>) -> Void) {
+    func login(completionHandler: (Swift.Result<URL, Error>) -> Void) {
         Authorization.resetCode()
         guard let url = URL(string: URLs.authorizationURL), let requestURL = url.encodeURL(withUrl: url, withMethod: .get, withParameters: Authorization.parameters) else {
-            completionHandler(.failure(.notURL))
+            completionHandler(.failure(APIError.notURL))
             return
         }
         completionHandler(.success(requestURL))
     }
-    func getAuthentication(with code: String, completionHandler: @escaping (Swift.Result<AccessTokenResponse, AuthorizationError>) -> Void) {
+    func getAuthentication(with code: String, completionHandler: @escaping (Swift.Result<AccessTokenResponse, Error>) -> Void) {
         
         guard let url = URL(string: URLs.authorizationAccessCodeURL) else {
-            completionHandler(.failure(.notURL))
+            completionHandler(.failure(APIError.notURL))
             return
         }
         Authorization.setCode(code: code)
@@ -43,8 +43,8 @@ class AuthorizationViewModel {
                 switch response.result {
                 case .success(let accessToken):
                     completionHandler(.success(accessToken))
-                case .failure( _):
-                    completionHandler(.failure(.failedRequest))
+                case .failure(let error):
+                    completionHandler(.failure(error))
                 }
         }
     }
