f2f8368c58bdeb301e96955fb7bf8b4aac431a11
andestaerfan <andesta.erfan@gmail.com>
2020-02-19 16:11:30 +0330

    - Implemented viewModel with getUser API

--- iOS-Challenge/Modules/Profile/ViewModels/ProfileViewModel.swift
+++ iOS-Challenge/Modules/Profile/ViewModels/ProfileViewModel.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import Alamofire
+
+class ProfileViewModel {
+    
+    private(set) var profile: Box<Profile?> = Box(Profile())
+    
+    func getProfile(completionHandler: @escaping ((Swift.Result<Void, Error>) -> Void)) {
+        guard let url = URL(string: URLs.userURL) else {
+            completionHandler(.failure(APIError.notURL))
+            return
+        }
+        Alamofire.request(url, method: .get, encoding: JSONEncoding.prettyPrinted, headers: [
+            "Accept": "application/json",
+            "Authorization": "token \(UserDefaultsService.shared.getToken() ?? "")"
+            ]).validate()
+            .responseDecodable { (response: DataResponse<Profile>) in
+                switch response.result {
+                case .success(let profile):
+                    self.profile.value = profile
+                    completionHandler(.success(()))
+                case .failure(let error):
+                    completionHandler(.failure(error))
+                }
+        }
+    }
+}
