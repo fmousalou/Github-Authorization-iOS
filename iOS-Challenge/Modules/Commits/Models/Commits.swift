e095a7e8242ac368e8c6ea754ef1e83b51a8c9fa
erfan andesta <andesta.erfan@gmail.com>
2020-02-20 11:10:40 +0330

    - Implemented directories and setup files for commits

---
+++ iOS-Challenge/Modules/Commits/Models/Commits.swift
@@ -0,0 +1,9 @@
+//
+//  Commits.swift
+//  iOS-Challenge
+//
+//  Created by erfan on 12/1/1398 AP.
+//  Copyright © 1398 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
---
+++ iOS-Challenge/Modules/Commits/ViewModels/CommitsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CommitsViewModel.swift
+//  iOS-Challenge
+//
+//  Created by erfan on 12/1/1398 AP.
+//  Copyright © 1398 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
