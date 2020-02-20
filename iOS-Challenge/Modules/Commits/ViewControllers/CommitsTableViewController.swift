2ea49173640bf09d263c9359e6e6c4b57f006160
erfan andesta <andesta.erfan@gmail.com>
2020-02-20 11:52:15 +0330

    - Implemented viewController for commits

--- iOS-Challenge/Modules/Commits/ViewControllers/CommitsTableViewController.swift
+++ iOS-Challenge/Modules/Commits/ViewControllers/CommitsTableViewController.swift
@@ -9,82 +9,64 @@
 import UIKit
 
 class CommitsTableViewController: UITableViewController {
-
+    
+    //MARK: - Vars -
+    private var customRefreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(getCommits), for: .valueChanged)
+        return refreshControl
+    }()
+    private var viewModel = CommitsViewModel(owner: "", repo: "")
+    //MARK: - View's LifeCycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupView()
+        getCommits()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    //MARK: - Functions -
+    
+    func setupContent(owner: String, repo: String) {
+        viewModel = CommitsViewModel(owner: owner, repo: repo)
     }
-
+    fileprivate func setupView() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.refreshControl = customRefreshControl
+    }
+    
+    @objc fileprivate func getCommits() {
+        viewModel.getCommits(completionHandler: { [weak self] (result) in
+            switch result {
+            case .success(_):
+                self?.tableView.reloadData()
+            case .failure(let error):
+                self?.handleError(error: error)
+            }
+            self?.refreshControl?.endRefreshing()
+            
+        })
+    }
+}
+//MARK: - TableView DataSource -
+extension CommitsTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return viewModel.commits.count
     }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell.textLabel?.text = viewModel.commits[indexPath.row].commit?.message
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+            cell.textLabel?.text = viewModel.commits[indexPath.row].commit?.message
+            cell.detailTextLabel?.text = viewModel.commits[indexPath.row].commit?.committer?.name
+            return cell
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+}
+//MARK: - TableView Delegate -
+extension CommitsTableViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
