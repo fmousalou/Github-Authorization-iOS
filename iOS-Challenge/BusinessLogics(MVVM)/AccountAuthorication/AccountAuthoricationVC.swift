b1f0a824f2c96f88d3babb3cd7ee15e24c28b2b5
Saeed Dehshiri <1995sd@gmail.com>
2020-04-12 18:55:19 +0400

    fix(structure): change to MVVM

---
+++ iOS-Challenge/BusinessLogics(MVVM)/AccountAuthorication/AccountAuthoricationVC.swift
@@ -0,0 +1,9 @@
+//
+//  AccountAuthoricationVC.swift
+//  iOS-Challenge
+//
+//  Created by Saeed Dehshiri on 4/12/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
